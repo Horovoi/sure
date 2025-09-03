1f984fe01bbd0b62b86d555f4e7fd0bc549d61d3
Mykyta Horovoi <mgorovoi@gmail.com>
2025-09-03 19:16:41 +0000

    Fix bug with a codependency of net worth chart and the sankey chart date selectors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,19 +25,22 @@ class PagesController < ApplicationController
         begin
           Period.from_key(period_param)
         rescue Period::InvalidKeyError
-          Period.last_30_days
+          # Fall back to the same period used elsewhere (user preference via Periodable)
+          @period
         end
       end
     else
-      Period.last_30_days
+      # Default to the globally selected period (from Periodable),
+      # which uses the current user's default when not provided via params
+      @period
     end
 
     family_currency = Current.family.currency
-    # Toggle: show/hide subcategories in Sankey (default: true)
+    # Toggle: show/hide subcategories in Sankey (default: false)
     @cashflow_show_subcategories = if params.key?(:cashflow_show_subcategories)
       ActiveModel::Type::Boolean.new.cast(params[:cashflow_show_subcategories])
     else
-      true
+      false
     end
     income_totals = Current.family.income_statement.income_totals(period: @cashflow_period)
     expense_totals = Current.family.income_statement.expense_totals(period: @cashflow_period)
